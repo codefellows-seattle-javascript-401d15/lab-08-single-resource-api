c9f71473ce1b6b2c55cf33251872101c60255ad7
Patrick Sheridan <sheridp2@gmail.com>
2017-04-27 13:01:44 -0700

    code working aside from PUT, need to added tests and README

--- lab-patrick/lib/router.js
+++ lab-patrick/lib/router.js
@@ -51,6 +51,7 @@ Router.prototype.route = function() {
       res.end();
     })
     .catch(err => {
+      console.error(err);
       res.writeHead(400, {'Content-Type': 'text/plain'});
       res.write('bad request');
       res.end();
