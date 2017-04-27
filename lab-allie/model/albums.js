0191ce4eb62dea5b6f104cff0518dcb917d4b48a
Allie Grampa <agrampa@yahoo.com>
2017-04-26 21:57:48 -0700

    post, put, get, and delete work yay :)

--- lab-allie/model/albums.js
+++ lab-allie/model/albums.js
@@ -4,9 +4,9 @@ const debug = require('debug')('http:albums');
 const uuid = require('uuid/v4');
 
 module.exports = function(artist, title, year, id) {
-  if(!artist || !title || !year || !id) throw new Error('Please enter a valid artist, title, year, and id');
-  this.artist = artist;
-  this.title = title;
-  this.year = year;
+  if(!artist || !title || !year) throw new Error('Please enter a valid artist, title, year, and id');
+  this.artist = artist,
+  this.title = title,
+  this.year = year,
   this.id = uuid();
 };
