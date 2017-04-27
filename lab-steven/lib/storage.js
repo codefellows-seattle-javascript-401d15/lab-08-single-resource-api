46213079a46582171ba1e1c566d6d7601b96b3a0
Steven Johnson <stevenjohnson86@gmail.com>
2017-04-27 12:45:51 -0700

    added api functionality

--- lab-steven/lib/storage.js
+++ lab-steven/lib/storage.js
@@ -1,1 +1,54 @@
 'use strict';
+
+const debug = require('debug')('#http:storage');
+const storage = {};
+
+module.exports = exports = {};
+
+exports.createHawk = function(schema, hawk){
+  debug('#createHawk');
+
+  if(!schema) return Promise.reject(new Error('schema required'));
+  if(!hawk) return Promise.reject(new Error('hawk required'));
+  if(!storage[schema]) storage[schema] = {};
+
+  storage[schema][hawk.id] = hawk;
+
+  return Promise.resolve(hawk);
+};
+
+exports.fetchHawk = function(schema, id){
+  debug('#fetchHawk');
+
+  return new Promise((resolve, reject) => {
+    if(!schema) return reject(new Error('schema required'));
+    if(!id) return reject(new Error('id required'));
+
+    let schemaName = storage[schema];
+    if (!schemaName) return reject(new Error('schema does not exist'));
+
+    let hawk = schemaName[id];
+    if (!hawk) return reject(new Error('hawk does not exist'));
+
+    return resolve(hawk);
+  });
+};
+
+exports.deleteHawk = function(schema, id){
+  debug('#deleteHawk');
+
+  return new Promise((resolve, reject) => {
+    if(!schema) return reject(new Error('schema required'));
+    if(!id) return reject(new Error('id required'));
+
+    let schemaName = storage[schema];
+    if(!schemaName) return reject(new Error('schema does not exist'));
+
+    let hawk = schemaName[id];
+    if(!hawk) return reject(new Error('hawk does not exist'));
+
+    delete storage[schema][id];
+
+    return(resolve(id));
+  });
+};
--- lab-steven/server.js
+++ lab-steven/server.js
@@ -1,1 +1,101 @@
 'use strict';
+
+const http = require('http');
+const Router = require('./lib/router');
+const storage = require('./lib/storage');
+const Seahawk = require('./model/seahawks');
+const debug = require('debug')('#http:server');
+const PORT = process.env.PORT || 3000;
+
+const router = new Router();
+const server = module.exports = http.createServer(router.route());
+
+router.get('/api/seahawk', function(req, res){
+  debug('GET /api/seahawk');
+  if(req.url.query.id) {
+    storage.fetchHawk('hawk', req.url.query.id)
+    .then(hawk => {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.write(JSON.stringify(hawk));
+      res.end();
+    })
+    .catch(err => {
+      console.log(err);
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('not found');
+      res.end();
+    });
+    return;
+  }
+  res.writeHead(400, {'Content-Type': 'text/plain'});
+  res.write('bad request');
+  res.end();
+});
+
+router.post('/api/seahawk', function(req, res){
+  debug('POST /api/seahawk');
+  console.log(req.body);
+  try {
+    let hawk = new Seahawk(req.body.name, req.body.pos, req.body.round);
+    storage.createHawk('hawk', hawk);
+
+    res.writeHead(201, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(hawk));
+    res.end();
+  } catch(err) {
+    res.writeHead(400, {'Content': 'text/plain'});
+    res.write('bad request');
+    res.end();
+  }
+});
+
+router.put('/api/seahawk', function(req, res){
+  debug('PUT /api/seahawk');
+  console.log(req.body);
+
+  if (req.url.query.id) {
+    storage.fetchHawk('hawk', req.url.query.id)
+    .then(hawk => {
+      if(req.body.name) hawk.name = req.body.name;
+      if(req.body.pos) hawk.pos = req.body.pos;
+      if(req.body.round) hawk.round = req.body.round;
+      res.writeHead(202, {'Content-Type': 'application/json'});
+      res.write(JSON.stringify(hawk));
+      res.end();
+    })
+    .catch(err => {
+      console.log(err);
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('not found');
+      res.end();
+    });
+    return;
+  }
+  res.writeHead(400, {'Content-Type': 'text/plain'});
+  res.write('bad request');
+  res.end();
+});
+
+router.delete('/api/seahawk', function(req, res){
+  debug('DELETE /api/seahawk');
+  if(req.url.query.id) {
+    storage.deleteHawk('hawk', req.url.query.id)
+    .then(id => {
+      console.log(id, 'deleted');
+      res.writeHead(204, {'Content-Type': 'text/plain'});
+      res.end();
+    })
+    .catch(err => {
+      console.log(err);
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('hawk not found');
+      res.end();
+    });
+    return;
+  }
+  res.writeHead(400, {'Content-Type': 'text/plain'});
+  res.write('bad request');
+  res.end();
+});
+
+server.listen(PORT, () => console.log(`Server listening at PORT: ${PORT}`));
